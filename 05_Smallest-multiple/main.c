559c531431b28c227aeb171458a0c525587b30a1
rigobertocanseco <cansecorigoberto@gmail.com>
2016-12-01 02:02:31 -0600

    5 int

---
+++ 05_Smallest-multiple/main.c
@@ -0,0 +1,14 @@
+//
+// Created by regoeco on 1/12/16.
+//
+/*
+    2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+    What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+ */
+#include <stdio.h>
+#include <math.h>
+int main(void) {
+    int i, num = 1;
+    // 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+    return 0;
+}
